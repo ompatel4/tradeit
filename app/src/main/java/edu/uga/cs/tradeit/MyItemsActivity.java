faad2079bf142f178ba4c4ff4882335c2fcc170e
Samhit Reddy <53240828+samhitreddy@users.noreply.github.com>
2025-12-01 22:01:58 -0500

    fixed errors

--- app/src/main/java/edu/uga/cs/tradeit/MyItemsActivity.java
+++ app/src/main/java/edu/uga/cs/tradeit/MyItemsActivity.java
@@ -6,22 +6,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.firebase.ui.database.SnapshotParser;
-
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.Query;
-
 import java.util.Map;
 
 /**
@@ -38,7 +34,7 @@ public class MyItemsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_items);
-        rvMyItems = findViewById(R.id.recyclerViewMyItems);  // Fixed ID match layout
+        rvMyItems = findViewById(R.id.recyclerViewMyItems);  // Fixed: match XML ID
         rvMyItems.setLayoutManager(new LinearLayoutManager(this));
 
         String uid = mAuth.getCurrentUser().getUid();
